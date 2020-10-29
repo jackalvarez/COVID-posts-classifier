3b5aa5f909a9e9afad8577d130f645e639b21934
Jostin Alvarez <alvarez.jostin@gmail.com>
2020-10-29 00:23:27 -0600

    Added the posts with the publications, plus the code to unite them in a single file with only the rows needed

---
+++ file_maker.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import csv
+import os
+
+index_df = pd.read_csv('posts_index.csv',  sep=',', encoding='utf-8', engine='python')
+
+print("\nPosts index: ")
+
+entries = os.listdir('./posts')
+
+full_df = pd.DataFrame(data=None)
+
+for entry in entries:
+	entry = "./posts/" + entry
+	print(entry)
+	df = pd.read_csv(entry,  sep=',', encoding='utf-8', engine='python', usecols=['URL','Message','Image Text','Link Text', 'Description'])
+	merged_data = index_df.merge(df,on=['URL'])
+	full_df = full_df.append(merged_data, ignore_index = True)
+
+print(full_df)
